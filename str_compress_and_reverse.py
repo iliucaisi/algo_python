e88a276f4812ac6bcf22a163af02d78be797b7da
Caisi LIU <liucaisilove@163.com>
2017-06-24 08:22:44 +0800

    String compress and revserse. Usage: python str_compress_and_reverse.py <string_with_spaces>

---
+++ str_compress_and_reverse.py
@@ -0,0 +1,30 @@
+import sys
+
+def compress_and_reverse(str1):
+	str1_len = len(str1)
+	i = 0
+	start = 0
+	compressed_and_reversed = []
+	skip = False
+	while i < str1_len:
+		if str1[i] == ' ' and skip == False:
+			compressed_and_reversed.append(str1[start:i][::-1])
+			skip = True
+		if skip == True and str1[i] != ' ':
+			start = i
+			skip = False
+		i = i + 1
+	if skip == False:
+		compressed_and_reversed.append(str1[start:i][::-1])
+
+	return " ".join(compressed_and_reversed)
+
+##################################################
+#				Main							 #
+##################################################
+# 												 #
+##################################################
+
+str1 = sys.argv[1]
+print compress_and_reverse(str1)
+
