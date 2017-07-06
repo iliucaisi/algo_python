e99ca869bf782f25310a9ad38ca9824eed132a08
Caisi LIU <liucaisilove@163.com>
2017-07-06 18:42:39 +0800

    Update with string finding no brute method. Usage: 	python str_matcher.py <str1> <str2>

--- str_matcher.py
+++ str_matcher.py
@@ -1,27 +1,20 @@
 import sys
 
-##################################################
-#				Main							 #
-##################################################			
-# 												 #
-##################################################
-str1 = sys.argv[1]
-str2 = sys.argv[2]
+def brute_match_with_wildcard(str1,str2):
+    str1_len = len(str1)
+    str2_len = len(str2)
 
-str1_len = len(str1)
-str2_len = len(str2)
+    i = 0
+    j = 0
+    skip = False
+    skip_count = 0
+    matched=[]
 
-i = 0
-j = 0
-skip = False
-skip_count = 0
-matched=[]
-
-if str1_len == 1 and str1[0] == "*":
+    if str1_len == 1 and str1[0] == "*":
 	print "Matched ", str2
 	exit(0)
 
-while (i < str1_len) and (j < str2_len):
+    while (i < str1_len) and (j < str2_len):
 	if str1[i] == '*':
 		skip = True
 		i = i + 1
@@ -40,9 +33,43 @@ while (i < str1_len) and (j < str2_len):
 		i = 0
 	
 	j = j + 1
-
-if skip == True:
+    if skip == True:
 	matched.append(str2[j:])
 
-print "Matched: ","".join(matched) 
-	
+    print "Matched: ","".join(matched) 
+
+def brute_match_without_wildcard(str1, str2):
+    '''
+    str1 - text string
+    str2 - pattern string
+    '''
+    len_str1 = len(str1)
+    len_str2 = len(str2)
+
+    i = 0
+    j = 0
+
+    while i < len_str1 and j < len_str2:
+        if str1[i] == str2[j]:
+            i += 1
+            j += 1
+        else:
+            i = i - j + 1
+            j = 0
+
+    if j == len_str2:
+        return i - j
+    else:
+        return -1
+
+
+##################################################
+#	        	Main    		 #
+##################################################			
+str1 = sys.argv[1]
+str2 = sys.argv[2]
+
+# brute_match_with_wildcard(str1, str2)
+
+print "Matched pattern index is: ", brute_match_without_wildcard(str1, str2)
+
